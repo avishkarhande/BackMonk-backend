06b799ae2f879c3afb22f974f2dc309e4a2eed9f
avishkarhande <avishkarhande@gmail.com>
2023-04-23 17:01:53 +0530

    Added Model Parser

---
+++ backend/base/model_parser.py
@@ -0,0 +1,43 @@
+list_params = {
+    "primary_key" : True,
+    "max_length": True,
+    "description": True,
+    'null': True,
+    'blank': True,
+    'default': True,
+    'choices': True,
+    'verbose_name': True,
+    'help_text': True,
+    'unique': True,
+    'primary_key': True,
+    'db_index': True,
+    'editable': True,
+    'max_length': True,
+    'upload_to': True,
+}
+
+django_model_types = {
+    "integer": "models.IntegerField",
+    "string": "models.CharField",
+    "text": "models.TextField",
+}
+
+def getParsedData(data):
+    for i in data["tables"]:
+        table_name = i["name"]
+        columns = i["columns"]
+        model_output = ""
+        class_name = "class " + table_name + "(models.Model):"
+        model_output += class_name + "\n"
+        for j in columns:
+            if j["type"] in django_model_types:
+                field_name = django_model_types[j["type"]]
+                params = "("
+                for param in list_params:
+                    if param in j:
+                        params += str(param) + "=" + str(j[param]) + ","
+                params += ")"
+                query = "    " + j["name"] + "=" + field_name + params
+                model_output += query + "\n"
+        print(model_output)
+    return data
--- backend/base/urls.py
+++ backend/base/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.endpoints),
     path('api/login', views.login),
     path('api/models', views.getAllModels),
-    path('api/register', views.register)
+    path('api/register', views.register),
+    path('api/model_generate', views.Model_generator)
 ]
